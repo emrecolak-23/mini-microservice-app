4f98dfb0e38f367e256da7dbe90d3e05bda6c2ac
Emre Çolak <emrecolak@Emre.home>
2023-03-09 23:40:47 +0300

    creating moderation service, adding comment moderation, handling moderation, updating comment content

--- comments/index.js
+++ comments/index.js
@@ -23,7 +23,7 @@ app.post('/posts/:id/comment', async (req, res) => {
 
   const comments = commentsByPostId[postId] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: 'pending' });
   commentsByPostId[postId] = comments;
 
   await axios.post('http://localhost:4005/events', {
@@ -32,14 +32,33 @@ app.post('/posts/:id/comment', async (req, res) => {
       id: commentId,
       content,
       postId: req.params.id,
+      status: 'pending',
     },
   });
 
   res.status(201).json(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Received Event: ', req.body.type);
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((comment) => comment.id === id);
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        content,
+        postId,
+        status,
+      },
+    });
+  }
   res.send({});
 });
 
