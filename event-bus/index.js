485e35c97319f73379f946dba13eb3c25ac14be6
Emre Çolak <emrecolak@Emre.home>
2023-03-12 21:52:03 +0300

    updating services address and verifying communication

--- event-bus/index.js
+++ event-bus/index.js
@@ -11,12 +11,12 @@ app.post('/events', (req, res) => {
   const event = req.body;
   events.push(event);
 
-  axios.post('http://localhost:4000/events', event);
-  axios.post('http://localhost:4001/events', event);
-  axios.post('http://localhost:4002/events', event).catch((error) => {
-    console.log(error.message);
-  });
-  axios.post('http://localhost:4003/events', event);
+  axios.post('http://posts-clusterip-srv:4000/events', event);
+  // axios.post('http://localhost:4001/events', event);
+  // axios.post('http://localhost:4002/events', event).catch((error) => {
+  //   console.log(error.message);
+  // });
+  // axios.post('http://localhost:4003/events', event);
 
   res.send({ status: 'OK' });
 });
